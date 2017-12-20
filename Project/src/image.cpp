5e88f2818f1ce9f8fd6c2c0921e4c4e35b36284b
Charly Girot <charly.girot@mastertp>
2017-12-20 14:52:48 +0100

    image a jour

--- Project/src/image.cpp
+++ Project/src/image.cpp
@@ -5,3 +5,72 @@ Image::Image()
 }
 
 
+
+void Image::imread(std::string image_path, std::string mask_path)
+{
+    cv::Mat image = cv::imread(image_path, CV_LOAD_IMAGE_COLOR);
+    cv::Mat mask  = cv::imread(mask_path, CV_LOAD_IMAGE_GRAYSCALE);
+
+    int const Nu = image.cols;
+    int const Nv = image.rows;
+
+    //Init mat alpha
+    alpha_data.zeros(Nv,Nu,CV_8UC1);
+    for(int i=0; i<Nu; ++i)
+        for(int j=0; j<Nv; ++j)
+        {
+
+        }
+}
+
+
+void Image::imwrite(std::string filepath)
+{
+
+}
+
+
+cv::Mat const& Image::image() const
+{
+    return image_data;
+}
+
+cv::Mat const& Image::alpha() const
+{
+    return alpha_data;
+}
+
+int const& Image::image(int u, int v) const
+{
+    return image_data.at<int>(u,v);
+}
+
+int const& Image::alpha(int u, int v) const
+{
+    return alpha_data.at<int>(u,v);
+}
+
+int& Image::image(int u, int v)
+{
+    return alpha_data.at<int>(u,v);
+}
+
+int& Image::alpha(int u, int v)
+{
+    return alpha_data.at<int>(u,v);
+}
+
+
+bool Image::is_border(int u, int v)
+{
+    int nb_out = 0;
+    for(int i=-1; i<=1 ; ++i)
+        for(int j=-1; j<=1; ++j)
+        {
+            if(image(u+i,v+j))
+            {
+                return true;
+            }
+        }
+    return false;
+}
--- Project/src/image.hpp
+++ Project/src/image.hpp
@@ -8,24 +8,48 @@
 #include "opencv2/opencv.hpp"
 #include <opencv2/core/core.hpp>
 
+enum
+{
+    OUT,
+    UPDATED,
+    BORDER,
+    IN
+};
 
 class Image : public cv::Mat
 {
 public:
     Image();
 
-    /** Read original image and mask then compute alphas */
+    /** Read original image and mask then compute alphas <==> Set Image and Set Alpha */
     void imread(std::string image_path, std::string mask_path);
 
     /** Write an image */
     void imwrite(std::string filepath);
 
+    /** Get Image */
+    cv::Mat const& image() const;
+    /** Get alpha Map */
+    cv::Mat const& alpha() const;
+    /** Get Image value at pixel u,v */
+    int const& image(int u, int v) const;
+    /** Get alpha value at pixel u,v */
+    int const& alpha(int u, int v) const;
+
+    /** Set Image value at pixel u,v */
+    int& image(int u, int v);
+    /** Set Alpha value at pixel u,v */
+    int& alpha(int u, int v);
+
+    /** Return if a pixel belong to the border */
+    bool is_border(int u, int v);
+
+
 private:
     /** Store the image to fill/restore */
-    cv::Mat image;
+    cv::Mat image_data;
     /** Store the alphas : 0->region to fill 1->border of the region 2->region already filled 3-> image source */
-    cv::Mat alpha;
-
+    cv::Mat alpha_data;
 };
 
 
