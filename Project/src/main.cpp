0b0f6b9319ee7f4a4f04c669d5d1d3d91df81732
Charly Girot <charly.girot@mastertp>
2018-01-24 09:46:33 +0100

    ajout d'images

--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -31,29 +31,32 @@ int main()
     /** Read Image */
     Image image = Image();
     /**Image de test */
-    //    image.imread("../../images/originales/oval_petit.png","../../images/mask/oval_petit_mask.png");
-//    image.imread("../../images/originazzles/oval_grand.png","../../images/mask/oval_grand_mask.png");
-//        image.imread("../../images/originales/linc.jpg","../../images/mask/linc-mask.bmp");
+//        image.imread("../../images/originales/oval_petit.png","../../images/mask/oval_petit_mask.png");
+//    image.imread("../../images/originales/oval_grand.png","../../images/mask/oval_grand_mask.png");
+        image.imread("../../images/originales/linc.jpg","../../images/mask/linc-mask.bmp");
 //        image.imread("../../images/originales/fillorder.png","../../images/mask/fillorder_mask.png");
 //        image.imread("../../images/originales/bungee0.png","../../images/mask/bungee0-mask.bmp"); //Not working - malloc corruption ...
-
+//image.imread("../../images/originales/concave.bmp","../../images/mask/concave.bmp");
     /** Image réelles */
 //            image.imread("../../images/originales/trolltunga.jpg","../../images/mask/trolltunga_mask.bmp");
 //        image.imread("../../images/originales/lakeandballoon.jpg","../../images/mask/lakeandballoon_mask.bmp"); //only balloon
     //    image.imread("../../images/originales/lakeandballoon.jpg","../../images/mask/lakeandballoon_mask2.bmp"); //ballon + ballon in water
-        image.imread("../../images/originales/text.jpg","../../images/mask/text-mask.bmp");
+//        image.imread("../../images/originales/text.jpg","../../images/mask/text-mask.bmp");
     //    image.imread("../../images/originales/bruandet.png","../../images/mask/bruandet-mask.bmp");
     //    image.imread("../../images/originales/bruandet2.png","../../images/mask/bruandet2-mask.bmp");
+//        image.imread("../../images/originales/Young_jaws_features.png","../../images/mask/Young_jaws_features.bmp");
+//    image.imread("../../images/originales/plane.jpg","../../images/mask/plane.bmp");
+
 
 
     /** Create the instance for the algorithm */
-    RegionFill region = RegionFill(25,25);
+    RegionFill region = RegionFill(7,7);
     /** Furnish the Image containing image and mask to the algorithm */
     region.set_image(&image);
     /** Run the Object Filling algorithm */
     region.run();
     /** Save the final result */
-    image.imwrite("resulText25.png");
+    image.imwrite("test.png");
 
     ~image;
 
--- opencv_project/inpaint_opencv.py
+++ opencv_project/inpaint_opencv.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 
 #LENA
-#img = cv2.imread('../images/standard_test_images/lena_color_256.png')
+#img = cv2.imread('../images/originales/lena_color_256.png')
 #d_img = cv2.imread('../images/standard_test_images/mask_lena_color_256.png')
 
 
@@ -10,20 +10,23 @@ import cv2
 #mask = cv2.imread('../images/standard_test_images/mask_peppers_color.png')
 
 #LINCOLN
-img = cv2.imread("../images/lincoln.jpg")
-d_img = cv2.imread("../images/d_lincoln.jpg")
+img = cv2.imread("../images/originales/lincoln.jpg")
+#d_img = cv2.imread("../images/d_lincoln.jpg")
 
 
 
-mask = d_img - img
+mask = cv2.imread("../images/mask/linc-mask.bmp")
 
 ret,thresh1 = cv2.threshold(mask,1,255,cv2.THRESH_BINARY)
 mask3 = cv2.cvtColor(thresh1,cv2.COLOR_RGB2GRAY, 1)
 dst = cv2.inpaint(img,mask3 ,3,cv2.INPAINT_TELEA) #INPAINT_TELEA or INPAINT_NS
 
 
-cv2.imshow('Originale', d_img)
+cv2.imshow('Originale', img)
+cv2.imshow('Mask',mask3)
 cv2.imshow('dst',dst)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+cv2.imwrite('resultLinc2OpenCV.jpg',dst)
 
